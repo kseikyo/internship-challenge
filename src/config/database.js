812365fdea26d9da7cb6580d78d055b9e8a8d4b2
Lucas Sierota <lucasemanuelss@hotmail.com>
2020-02-05 16:00:27 -0300

    Created database.js
    The configuration for connecting to the database

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // URI: "postgres://ijsfeawc:XBxJBzn1RsKs-6Q66PrUFEeWQOn4oYcf@tuffi.db.elephantsql.com:5432/ijsfeawc",
+  dialect: 'postgres',
+  host: 'tuffi.db.elephantsql.com',
+  port: '5432',
+  username: 'ijsfeawc',
+  password: 'XBxJBzn1RsKs-6Q66PrUFEeWQOn4oYcf',
+  database: 'ijsfeawc',
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
