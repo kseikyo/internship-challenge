2970f7bb9ef302d90486bcd6fb9ff993b2a52be3
Lucas <Lucas@DESKTOP-2EG4LON.local>
2020-02-09 19:14:08 -0300

    Update package-lock.json, package.json, and 7 more files...
    Added comments, improved default values of the form, removed bootstrap dependency and refactored folder structures.

--- frontend/src/components/misc/changeHandler.js
+++ frontend/src/components/misc/changeHandler.js
@@ -1,10 +1,17 @@
 export function changeHandler(event, callback = null) {
+  /* The changeHandler
+   *
+   * This handler will set the state of anything inside the formControls
+   * The event may be null when typing the date
+   * If the event is not null, if it doesn't have a target, it's a date event
+   * Else, it will take the name and the value and set it to it's proper state 
+   */
   if (!event) return;
   else if (!event.target) {
     this.setState({
       formControls: {
         ...this.state.formControls,
-        birthdate: event.toISOString()
+        birthdate: event
       }
     })
   } else {
